0a5e8bbc75847370371e3dd7ff292e27eae5bd71
Dev-Maidu <maidulislammanik8991@gmail.com>
2025-08-31 04:05:48 +0800

    Learn array,tupple and union datatypes

---
+++ index.js
@@ -0,0 +1,24 @@
+// union data type= string or number multiple data types
+// let userId:(string | number | boolean);
+// userId=101;
+// userId="202";
+// userId=false;
+// function displayUserInfo(userId: string|number|boolean){
+//     console.log(userId);
+// }
+// displayUserInfo(101);
+// displayUserInfo(false)
+//==================== Array data type======================
+// let userNames:Array<string>;
+// userNames=["maidul","heryy"]
+// console.log(userNames);
+// let userId : (string|number)[];
+// userId=["101",444];
+// console.log(userId);
+//==================== Tuple data type======================
+// mixed data type key:value pairs
+var user;
+user = [101, "Maidul"];
+console.log(user);
+user.push(102, "Jahid");
+console.log(user);
